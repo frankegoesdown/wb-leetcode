3198c79e295962e5d9088c353cbb2062871c386b
Alexander Chernikov <Chernikov.A4@wildberries.ru>
2020-04-28 23:38:11 +0300

    0007

---
+++ 0007-reverse-integer/reverse_integer.go
@@ -0,0 +1,14 @@
+package reverse_integer
+
+import "math"
+
+func reverse(x int) (result int) {
+	for x != 0 {
+		result = result*10 + x%10
+		if result < math.MinInt32 || result > math.MaxInt32 {
+			return 0
+		}
+		x = x / 10
+	}
+	return
+}
